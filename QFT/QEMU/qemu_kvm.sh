03c1445ea0ce55608686c4e7fce15d38e07dde4e
fmbfs <franciscombfsantos@gmail.com>
2022-04-13 09:33:30 +0100

    cleaned

---
+++ QFT/QEMU/qemu_kvm.sh
@@ -0,0 +1,163 @@
+
+#
+#
+#
+##################################################
+#		Improvments to be done:
+#		- Make a formula to automaticly calculate
+#		the best cluster and l2_cache
+##################################################
+#
+#
+#
+
+#--------------------------------------------------------------------
+#!/bin/bash
+
+# Default
+#BASE_DIR is the path where .sh is
+BASE_DIR="${PWD}"
+ARGUMENT1="$1"
+ARGUMENT2="$2"
+
+#--------------------------------------------------------------------
+#FUNCTIONS
+
+set_variables(){
+	#OS .iso Paths
+	IMAGES_DIR="${BASE_DIR}/Iso_Images/Windows"
+
+	#Virtual disks (VD) path
+	QEMU_VD="${BASE_DIR}/Virtual_Disks"
+
+	## QEMU name and OS --> Windows 10
+	OS_ISO="${IMAGES_DIR}/Win10_21H2_English_x64.iso"
+	VD_NAME="${ARGUMENT2}.qcow2"
+	OS_IMG="${QEMU_VD}/${VD_NAME}"
+
+    # Processor
+    CORES="4"
+	THREADS="2"
+	# IMAGE
+	Disk_Size="40G"
+	Cluster_Size="64K"
+	L2_Cache_Size="5M"
+    #1Mb for 8Gb using 64Kb
+    #CACHE CLEAN IN SECONDS
+	Cache_Clean_Interval="60"
+	 # RAM
+    VD_RAM="8G"  
+
+	#Pinned CPU
+	CPU_PINNED="7"
+
+	#QEMU ARGUMENTS
+	QEMU_ARGS=(
+				"-name" "${ARGUMENT2}" \
+				"-cpu" "max,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time" \
+				"-smp" "cores=${CORES},threads=${THREADS}" \
+				"-enable-kvm" \
+				"-machine" "accel=kvm" \
+				"-m" "${VD_RAM}" \
+				"-rtc" "base=localtime,clock=host" \
+				"-drive" "file=${OS_IMG},l2-cache-size=${L2_Cache_Size},cache=writethrough,cache-clean-interval=${Cache_Clean_Interval}" \
+			)
+}
+
+#HELP MENU
+show_help(){
+	echo ""
+    echo "./create_system.sh [options]"
+    echo "Options:"
+    echo "  -osi -> Install the OS via CDROM"
+    echo "  -osc -> Creates a qcow2 image for OS"
+    echo "  -osl -> Launch qemu OS machine."
+    echo "  -help -> Show this help."
+    echo ""
+    exit 0
+}
+
+#SWITHC ARGUMENTS
+process_args(){
+	case "${ARGUMENT1}" in
+	"")
+		echo "No arguments provided,check below. "
+		show_help
+		shift
+		;;
+	"-osi")
+		os_install
+		shift
+		;;
+	"-osc")
+		create_image_os
+		shift
+		;;
+	"-osl")
+		os_launch
+		shift
+		;;
+	"-help")
+		show_help
+		shift
+		;;
+	*)
+		echo "Unrecognised option"
+		shift
+		;;
+	esac	
+}
+
+#CHECK IF FILE ALREADY EXISTS
+check_file(){
+	# Scenario - File exists and is not a directory
+	if test -f "$OS_IMG";
+	then
+		echo "${OS_IMG} exists!"
+		while true; 
+		do
+    		read -p "Do you want to overwrite? [y/n] " yn
+    		case $yn in
+        		[Yy]* ) echo "${OS_IMG} Overwritten!"; break;;
+        		[Nn]* ) exit 0;;
+        		* ) echo "Please answer yes or no.";;
+    		esac
+		done
+	else
+		echo "${OS_IMG} created!"
+	fi
+}
+
+#CREATE VIRTUAL DISK IMAGE
+create_image_os(){
+	echo "Creating Virtual Disk...";
+	qemu-img create -f qcow2 -o cluster_size=$Cluster_Size,lazy_refcounts=on $OS_IMG $Disk_Size
+	exit 1;
+}
+
+#LAUNCH QEMU-KVM
+os_launch(){
+	cd ${IMAGES_DIR}
+	echo "Launching OS..."
+    echo "$QEMU_ARGS"
+	#hexadecimal afinity for cpu placement
+    taskset -c $CPU_PINNED \
+    qemu-system-x86_64 $QEMU_ARGS
+	exit 1;
+}
+
+#INTALL THE OPERATING SYSTEM N THE VIRTUAL MACHINE
+os_install(){
+	cd ${IMAGES_DIR}
+	echo "Installing OS...";
+	taskset -c $CPU_PINNED \
+	qemu-system-x86_64 $QEMU_ARGS \
+    -cdrom ${OS_ISO}
+	exit 1;
+}
+
+#--------------------------------------------------------------------
+#MAIN
+set_variables
+process_args
+check_file
