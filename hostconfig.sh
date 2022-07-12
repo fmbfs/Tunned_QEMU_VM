12a8c808801558d4077b483b6bf41f10dabf6cc9
dummy tests <franciscombfsantos@gmail.com>
2022-07-12 15:28:47 +0100

    segmented

--- hostconfig.sh
+++ hostconfig.sh
@@ -33,8 +33,6 @@ BASE_DIR=$(dirname "${BASH_SOURCE[0]}")
 [[ "${BASE_DIR}" == "." ]] && BASE_DIR=$(pwd)
 
 # Arguments fishing from config file:
-# Name of the Virtual machine (VM)
-ARG1=$(config_fetching "Name")
 
 # RAM for VM
 VD_RAM=$(config_fetching "RAM")
@@ -45,89 +43,8 @@ small_pages="2048"
 # Boot Logs file
 boot_logs_path="${BASE_DIR}/boot_logs.txt"
 
-# Grab disk size 
-VSD_path="${BASE_DIR}/Tunned_VM/QFT/Virtual_Disks"
-cd ${VSD_path}
-Disk_Size=$(du -h ${VD_NAME} | awk '{print $1}' | cut -d 'G' -f1)
-cd ${BASE_DIR}
-
-set_variables(){
-	# QEMU name
-	VD_NAME="${ARG1}.qcow2"
-	OS_IMG="${VSD_path}/${VD_NAME}"
-    
-	# Process clusters
-	process_cluster
-
-    # Cache Clean interval in seconds
-	Cache_Clean_Interval=$(config_fetching "Cache Clean")
-	# Pinned vCPU
-	vCPU_PINNED=$(cat /sys/devices/system/cpu/cpu*/topology/thread_siblings_list | sort | uniq | tail -1)
-
-	# Common Args
-	QEMU_ARGS=(
-        "-name" "${ARG1}" \
-        "-enable-kvm" \
-        "-m" "${VD_RAM}G" \
-	)
-
-	# Specific Args
-	QEMU_ARGS+=(
-		"-cpu" "max,pdpe1gb,kvm=off,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time" \
-		"-mem-path" "/dev/hugepages" \
-		"-mem-prealloc" \
-		"-machine" "accel=kvm,kernel_irqchip=on" \
-		"-rtc" "base=localtime,clock=host" \
-		"-drive" "file=${OS_IMG},l2-cache-size=${L2_Cache_Size},cache=writethrough,cache-clean-interval=${Cache_Clean_Interval}" \
-	)
-}
-
-# Delete cset prevously created
-delete_cset(){
-    sudo cset set -d system
-    while [[ $(sudo cset set -d system) =~ "done" ]]; do 
-        sudo cset set -d system
-    done
-    sudo cset set -d user
-}
-
-# Scheduler FIFO processes
-sched(){
-    while :; do
-        if [ -f  ${boot_logs_path} ]; then
-            while IFS= read -r line; do
-                cur_bytes=$(echo ${line} | awk '{print $5}')
-                if [[ ${cur_bytes} != '512' ]]; then
-                    # Get parent PID of QEMU VM                                
-                    PARENT_PID=$(pstree -pa $(pidof qemu-system-x86_64) | grep ${ARG1} | cut -d','  -f2 | cut -d' ' -f1)
-                    # Set all threads of parent PID to SCHED_FIFO 99 priority
-                    pstree -pa $PARENT_PID | cut -d','  -f2 | cut -d' ' -f1 | xargs -L1 echo "chrt -f -p 99" | bash
-                    exit 0
-                fi
-            done < ${boot_logs_path}
-        fi
-    done 
-}
-
-# Process Cluster Sizes
-process_cluster(){
-    arr_cs_valid=("64","128","256","512","1024","2048")
-	# Virtual Storage Device (VSD) -- virtual hard drive size in GiB
-    # is automatically grabbed from QCOW2 file
-    # Cluster Size in KiB
-	cluster_size_value=$(config_fetching "VSD Cluster")
-	Cluster_Size="${cluster_size_value}K"
-	L2_calculated=0
-	if [[ "${arr_cs_valid[@]}" =~ "${cluster_size_value}" ]]; then
-		auxiliar_calc=$(( ${cluster_size_value}/8 ))
-		L2_calculated=$(( ${Disk_Size}/${auxiliar_calc} + 1 ))
-	else
-		echo "Invalid Cluster Size. Edit value in config.json"
-        echo "64, 128, 256, 512, 1024, 2048"
-        exit 0
-	fi
-	L2_Cache_Size="${L2_calculated}M"
-}
+# Pinned vCPU
+vCPU_PINNED=$(cat /sys/devices/system/cpu/cpu*/topology/thread_siblings_list | sort | uniq | tail -1)
 
 # Huge Pages set-up.
 page_size(){
@@ -188,7 +105,9 @@ grubsm(){
             echo "Already updated."
         else
             sudo sed -i "s/${default_arg}/${grub_tuned}/" ${grub_path}
-            sudo update-grub && shutdown -r now
+            
+            sudo update-grub
+            echo "You need to reboot your HOST!"
         fi
     elif [[ ${update_grub} == "no" ]]; then
         grub_tuned=$(cat ${grub_path} | grep "GRUB_CMDLINE_LINUX_DEFAULT=")
@@ -196,25 +115,22 @@ grubsm(){
             echo "Already default."
         else
             sudo sed -i "s/${grub_tuned}/${grub_default}/" ${grub_path}
-            sudo update-grub && shutdown -r now
+            sudo update-grub
+            echo "You need to reboot your HOST!"
         fi
     fi
 }
 
 # LAUNCHER for VM
-os_launch_tuned(){
-    ################################ PART I ##############################
-	echo "Launching VM..."
+setup(){
 	# Set cpu as performance
 	for file in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor; do 
         echo "performance" > $file
     done
 
-    # Runnig in parallel process priority scheduler
-	sched &
-
 	# Sched_rt_runtime_us to 98%
 	sysctl kernel.sched_rt_runtime_us=980000 >/dev/null
+
     
     # Call grub updater set the HugePages and run qemu with correct parameters
 	page_size >/dev/null
@@ -224,10 +140,9 @@ os_launch_tuned(){
     # Just add this to the grub file: GRUB_CMDLINE_LINUX="systemd.unified_cgroup_hierarchy=0"
     # Creating isolated set to launch qemu
     sudo cset shield --cpu=${vCPU_PINNED} --threads --kthread=on >/dev/null 
-    # Run VM the -d is to detect when windows boots
-    sudo cset shield -e \
-    qemu-system-x86_64 -- ${QEMU_ARGS[@]} -d trace:qcow2_writev_done_part 2> ${boot_logs_path} >/dev/null
+}
 
+unsetup(){
     ################################ PART III ##############################
 	# Free resources
 	echo "Freeing resources..."
@@ -251,16 +166,13 @@ os_launch_tuned(){
 ##### MAIN #####
 #####################################################################################################################################
 
-main(){
-	check_su
-	set_variables
-	os_launch_tuned
+#check_su
+if [[ ${1} == "--setup"]]; then
+    setup
+elif [[ ${1} == "--unsetup"]]; then
+    unsetup
+else
+    echo "Error: Missing argument (--setup or --unsetup)."
+fi
 
-	echo "Exit success!"
-}
-
-#####################################################################################################################################
-##### RUN #####
-#####################################################################################################################################
-
-main
+echo "Exit success!"
